5689a6dad773e122c17ff06cdb44755c14225cce
xiaotian132 <13292979265@163.com>
2025-02-22 14:04:13 +0800

    thrid save

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 功能：
-批量修改指定文件夹下所有.stp文件，名称依次为01.stp、02.stp、...
+批量修改指定文件夹下所有.stp文件，将名称依次加入-01.stp、02.stp、...的后缀；
+如：
+filea.stp ====> filea-01.stp
+fileb.stp ====> fileb-02.stp
+filec.stp ====> filec-03.stp
+...
 
 运行方式（Linux）：
-./mian inputPath
-其中inputPath是需要输入的文件夹路径
+执行指令：./main
+随后可连续输入处理路径
+Please input path:
+输入你的路径
+处理结束后，输入 ExitMain 退出
+
--- main.cpp
+++ main.cpp
@@ -2,23 +2,27 @@
 
 int main(int argc, char *argv[])
 {
-    // 错误判定
-    if (argc != 2)
+    while (true)
     {
-        return -1;
+        std::string inputPath;
+        std::cout << "Please input path: " << std::endl;
+        std::cin >> inputPath;
+        std::cout << "You Input Path: " << inputPath << std::endl;
+
+        if (inputPath == "ExitMain")
+        {
+            std::cout << "****ChangeFileName over****!" << std::endl;
+            break;
+        }
+
+        // 实例化修改文件名称
+        FindUpDate f1(inputPath);
+
+        // 批量修改文件名称
+        f1.ChangeFileName();
+
+        std::cout << "************************************************" << std::endl;
     }
-
-    // 获取输入路径
-    std::string inputPath = argv[1];
-
-    // 打印输入文件路径
-    std::cout << "Input path: " << inputPath.c_str() << std::endl;
-
-    // 实例化修改文件名称
-    FindUpDate f1(inputPath);
-
-    // 批量修改文件名称
-    f1.ChangeFileName();
 
     return 0;
 }
